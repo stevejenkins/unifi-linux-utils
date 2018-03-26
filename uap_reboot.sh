a83067b93a1fd89c5f31f402ee7f81d303d8cd15
Steve Jenkins <steve@stevejenkins.com>
2018-03-26 16:36:26 -0700

    Remove quotes from UAP list
    Solves #15.

--- uap_reboot.sh
+++ uap_reboot.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # A simple script for remotely rebooting a Ubiquiti UniFi access point
-# Version 2.1 (Jan 10, 2017)
+# Version 2.2 (Mar 26, 2018)
 # by Steve Jenkins (http://www.stevejenkins.com/)
 
 # Requires sshpass (https://sourceforge.net/projects/sshpass/) which
@@ -10,14 +10,14 @@
 # USAGE
 # Update the user-configurable settings below, then run ./uap_reboot.sh from
 # the command line. To reboot on a schedule, create a cronjob such as:
-# 45 3 * * * /usr/local/bin/uap_reboot.sh > /dev/null 2>&1 #Reboot UniFi APs
+# 45 3 * * * /usr/local/bin/unifi-linux-utils/uap_reboot.sh > /dev/null 2>&1 #Reboot UniFi APs
 # The above example will reboot the UniFi access point(s) every morning at 3:45 AM.
 
 # USER-CONFIGURABLE SETTINGS
 username=ubnt
 password=ubnt
 known_hosts_file=/dev/null
-uap_list=( "192.168.0.2" "192.168.0.3" )
+uap_list=( 192.168.0.2 192.168.0.3 )
 
 # SHOULDN'T NEED TO CHANGE ANYTHING PAST HERE
 for i in "${uap_list[@]}"
