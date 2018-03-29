0e4b6a3440f1a35f3027b0f12fb64e244de66f65
Steve Jenkins <steve@stevejenkins.com>
2018-03-28 20:53:48 -0700

    Specified bash to prevent Ubuntu syntax errors
    - Changed interpreter to Bash for consistency across RedHat and Debian distros
    - Added quotes to variable in sshpass command per ShellCheck
    
    Signed-off-by: Steve Jenkins <steve@stevejenkins.com>

--- uap_reboot.sh
+++ uap_reboot.sh
@@ -1,30 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
+#######################################################################
 # A simple script for remotely rebooting a Ubiquiti UniFi access point
-# Version 2.2 (Mar 26, 2018)
+# Version 2.3 (Mar 28, 2018)
 # by Steve Jenkins (http://www.stevejenkins.com/)
-
-# Requires sshpass (https://sourceforge.net/projects/sshpass/) which
-# is probably available via dnf, yum, or apt on your *nix distro.
-
+#
+# Requires bash and sshpass (https://sourceforge.net/projects/sshpass/)
+# which should be available via dnf, yum, or apt on your *nix distro.
+#
 # USAGE
 # Update the user-configurable settings below, then run ./uap_reboot.sh from
 # the command line. To reboot on a schedule, create a cronjob such as:
 # 45 3 * * * /usr/local/bin/unifi-linux-utils/uap_reboot.sh > /dev/null 2>&1 #Reboot UniFi APs
 # The above example will reboot the UniFi access point(s) every morning at 3:45 AM.
+#######################################################################
 
 # USER-CONFIGURABLE SETTINGS
 username=ubnt
 password=ubnt
 known_hosts_file=/dev/null
-uap_list=( 192.168.0.2 192.168.0.3 )
+uap_list=( 192.168.0.2 192.168.0.3 192.168.0.4 192.168.0.5 )
 
 # SHOULDN'T NEED TO CHANGE ANYTHING PAST HERE
 for i in "${uap_list[@]}"
 do
 
 	echo "Rebooting UniFi access point at $i..."
-	if sshpass -p $password ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=$known_hosts_file $username@$i reboot; then
+	if sshpass -p $password ssh -oStrictHostKeyChecking=no -oUserKnownHostsFile=$known_hosts_file $username"@$i" reboot; then
                 echo "Access point at $i rebooted!" 1>&2
 	else
                 echo "Could not reboot access point at $i." 1>&2
